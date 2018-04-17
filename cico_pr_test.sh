7591ed7344613245886e3246470b2f5f31992224
Budh Ram Gurung <budhram.gurung01@gmail.com>
2018-04-17 16:36:10 +0530

    Issue #60 Fix installation of jenkins-job-builder for pr test

--- cico_pr_test.sh
+++ cico_pr_test.sh
@@ -2,4 +2,11 @@
 
 set -ex
 
+yum -y update
+yum -y install epel-release
+yum -y install python-pip
+# Install jenkins-jobs
+pip install --user jenkins-job-builder
+# Pip install jenkins-jobs binary into $HOME/.local/bin
+export PATH=$HOME/.local/bin:$PATH
 jenkins-jobs test minishift-ci-index.yaml > /dev/null
